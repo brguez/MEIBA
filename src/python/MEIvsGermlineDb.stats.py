d9e026e20cfc8def203e14edc0e8781ae50c15b2
brguez <rodriguezmartinbernardo@gmail.com>
2016-09-01 13:33:43 +0200

    MEIvsGermlineDb.stats.py Added scatterplot and correlation analysis between 1K-GENOMES and PCAWG MEI length

--- src/python/MEIvsGermlineDb.stats.py
+++ src/python/MEIvsGermlineDb.stats.py
@@ -6,7 +6,7 @@ def header(string):
         Display  header
     """ 
     timeInfo = time.strftime("%Y-%m-%d %H:%M")
-    print '\n', timeInfo, "****", string, "****"
+    print timeInfo, "****", string, "****"
 
 def info(string):
     """ 
@@ -135,18 +135,29 @@ print
 print "***** Executing ", scriptName, ".... *****"
 print 
 
-
+###########
 ## Start ## 
+###########
 
-## 1. Create VCF object and read input VCF
+############################################
+## 1. Create VCF object and read input VCF #
+############################################
+header("Read input VCF")
 VCFObj = formats.VCF()
 VCFObj.read_VCF(VCF)
 
-## 2. Create germline MEI database object
+###########################################
+## 2. Create germline MEI database object #
+###########################################
+header("Read germline MEI database")
 germlineMEIDbObj = germlineMEIDb()
 germlineMEIDbObj.read_bed(germlineMEIBed)
 
-## 3. Compare TraFiC MEI characteristics with matching 1000G MEI
+##################################################################
+## 3. Compare TraFiC MEI characteristics with matching 1000G MEI #
+##################################################################
+header("Compare PCAWG with 1K-GENOMES MEI")
+
 # Gather all this information into arrays for plotting. 
 bkpDistances = [] 
 strandComparisons = []
@@ -240,12 +251,9 @@ for VCFlineObj in VCFlineObjList:
 			# d) transposon length 
 			if (MEI1000G.length != "UNK") and ('LEN' in VCFlineObj.infoDict):
 
-				## note: substract 31, since it is the length of the polyA sequence added to the  Alu 
-				# and SVA reference sequences. For L1, the length is 33... This is temporal. I need to modify 'findInsertionBkp' script
-				# to substract polyA length internally. 
-				length = int(VCFlineObj.infoDict['LEN']) - 32		
+				length = int(VCFlineObj.infoDict['LEN']) 	
 				difference = abs(int(MEI1000G.length) - length) 
-				lenTuple = ( length, MEI1000G.length )
+				lenTuple = ( length, int(MEI1000G.length) )
 
 				if (VCFlineObj.infoDict['CLASS'] == "L1"):			
 					MEILenL1Distances.append(difference)				
@@ -267,8 +275,14 @@ for VCFlineObj in VCFlineObjList:
 	else:
 		print 'no'		
 
-## 3. Plot information gathered in 2)
+#######################################
+## 3. Plot information gathered in 2) #
+#######################################
+
 ## 3.1 Bkp distances, strand and TSD consistency/inconsistency bar plot
+########################################################################
+header("Make barplot")
+
 ## 3.1.1 Gather information
 ## Bkp distances 
 # Count number of consistent and inconsistent bkp instances
@@ -305,14 +319,12 @@ consistentList = [ bkpDict['consistent'], TSDDict['consistent'], strandDict['con
 inconsistentList = [ bkpDict['inconsistent'], TSDDict['inconsistent'], strandDict['inconsistent'] ]
 #print "TSD-len-distances: ", sorted(TSDLenDistances), np.mean(TSDLenDistances), np.std(TSDLenDistances), TSDDict
 
-print consistentList, inconsistentList
-
 ## 3.1.3 Make figure
 # Make bar plot 
 xpos = np.arange(3)    # the x locations for the groups
 width = 0.5       # the width of the bars: can also be len(x) sequence
 fig = plt.figure(figsize=(5,6))
-fig.suptitle('PCAWG and 1K-GENOMES consistency')
+fig.suptitle('MEI features consistency')
 plt.ylabel('%', fontsize=12)
 ax = fig.add_subplot(111)
 p1 = ax.bar(xpos, consistentList, color='#008000', alpha=0.5, edgecolor='#000000', width=width, align='center')
@@ -333,7 +345,11 @@ fileName = outDir + "/PCAWG_1KGENOMES_barPlot.pdf"
 plt.savefig(fileName)
 
 ## 3.2 Transposon length distances correlation plot
-fig = plt.figure(figsize=(5,6))  
+#####################################################
+header("Compute correlations and make scatterplots")
+
+fig = plt.figure(figsize=(10,14))                
+fig.suptitle('MEI length correlation', fontsize=18)
 
 ## L1
 # print "L1-len-distances: ", sorted(MEILenL1Distances), np.mean(MEILenL1Distances), np.std(MEILenL1Distances)
@@ -341,15 +357,43 @@ tmpList = map(list, zip(*MEILenL1))
 L1lenPCAWG = tmpList[0]
 L1len1000G = tmpList[1]
 
+# Compute correlation
+corr = stats.pearsonr(L1lenPCAWG, L1len1000G)
+coefficient = format(corr[0], '.3f') 
+pvalue = corr[1]
+text = 'pearson_corr: ' + str(coefficient)
+
+# Make scatterplot
+ax1 = fig.add_subplot(2, 2, 1)
+ax1.set_title("LINE-1", fontsize=14)
+plt.scatter(L1lenPCAWG, L1len1000G, color='#008000', alpha=.4)
+plt.xlim((0, (max(L1lenPCAWG) + 500)))
+plt.ylim((0, (max(L1len1000G) + 600)))
+plt.xlabel('PCAWG', fontsize=12)
+plt.ylabel('1K-GENOMES', fontsize=12)
+ax1.text(0.5, 0.1, text, transform = ax1.transAxes)
+
 ## Alu
 # print "Alu-len-distances: ", sorted(MEILenAluDistances), np.mean(MEILenAluDistances), np.std(MEILenAluDistances)
 tmpList = map(list, zip(*MEILenAlu))
 AlulenPCAWG = tmpList[0]
 Alulen1000G = tmpList[1]
 
-plt.scatter(AlulenPCAWG, Alulen1000G)
-plt.show()
-print "Alu-corr: ", stats.pearsonr(AlulenPCAWG, Alulen1000G)
+# Compute correlation
+corr = stats.pearsonr(AlulenPCAWG, Alulen1000G )
+coefficient = format(corr[0], '.3f') 
+pvalue = corr[1]
+text = 'pearson_corr: ' + str(coefficient)
+
+# Make scatterplot
+ax2 = fig.add_subplot(2, 2, 2)
+ax2.set_title("ALU", fontsize=14)
+plt.scatter(AlulenPCAWG, Alulen1000G, color='#008000', alpha=.4)
+plt.xlim((0, (max(AlulenPCAWG) + 10)))
+plt.ylim((0, (max(Alulen1000G) + 15)))
+plt.xlabel('PCAWG', fontsize=12)
+plt.ylabel('1K-GENOMES', fontsize=12)
+ax2.text(0.5, 0.1, text, transform = ax2.transAxes)
 
 ## SVA 
 # print "SVA-len-distances: ", sorted(MEILenSVADistances), np.mean(MEILenSVADistances), np.std(MEILenSVADistances)
@@ -357,15 +401,36 @@ tmpList = map(list, zip(*MEILenSVA))
 SVAlenPCAWG = tmpList[0]
 SVAlen1000G = tmpList[1]
 
+# Compute correlation
+corr = stats.pearsonr(SVAlenPCAWG, SVAlen1000G )
+coefficient = format(corr[0], '.3f') 
+pvalue = corr[1]
+text = 'pearson_corr: ' + str(coefficient)
+
+# Make scatterplot
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.set_title("SVA", fontsize=14)
+plt.scatter(SVAlenPCAWG, SVAlen1000G, color='#008000', alpha=.4)
+plt.xlim((0, (max(SVAlenPCAWG) + 100)))
+plt.ylim((0, (max(SVAlen1000G) + 150)))
+plt.xlabel('PCAWG', fontsize=12)
+plt.ylabel('1K-GENOMES', fontsize=12)
+ax3.text(0.5, 0.1, text, transform = ax3.transAxes)
+
+## Save figure
+fileName = outDir + "/PCAWG_1KGENOMES_correlation.pdf"
+plt.savefig(fileName)
+
 ## 3.3 Venn diagram
-info("Plot PCAWG vs 1KGENOMES MEI venn diagrams\n")
+######################
+header("Make venn diagrams")
 
 # Generate needed data
 classesList1000G = [line.rstrip('\n').split('\t')[3] for line in open(germlineMEIBed)]
 
 # Make figure
 fig = plt.figure(figsize=(5,6))                
-fig.suptitle('Overlap between MEI germline calls')
+fig.suptitle('Overlap between MEI calls')
 
 ## L1
 # Generate needed data
@@ -374,7 +439,7 @@ L1Both = sum(1 for VCFlineObj in VCFlineObjList if (VCFlineObj.infoDict['CLASS']
 totalL11000G = classesList1000G.count("L1")
 L11000G = totalL11000G - L1Both
 
-# Plot venn diagram
+# Make venn diagram
 ax1 = fig.add_subplot(2, 2, 1)
 ax1.set_title("LINE-1", fontsize=10)
 venn2(subsets=(L1PCAWG, L11000G, L1Both), set_labels = ('', ''))
@@ -386,9 +451,9 @@ AluBoth = sum(1 for VCFlineObj in VCFlineObjList if (VCFlineObj.infoDict['CLASS'
 totalAlu1000G = classesList1000G.count("Alu")
 Alu1000G = totalAlu1000G - AluBoth
 
-# Plot venn diagram
-ax1 = fig.add_subplot(2, 2, 2)
-ax1.set_title("ALU", fontsize=10)
+# Make venn diagram
+ax2 = fig.add_subplot(2, 2, 2)
+ax2.set_title("ALU", fontsize=10)
 venn2(subsets=(AluPCAWG, Alu1000G, AluBoth), set_labels = ('', ''))
 
 ## SVA
@@ -398,9 +463,9 @@ SvaBoth = sum(1 for VCFlineObj in VCFlineObjList if (VCFlineObj.infoDict['CLASS'
 totalSva1000G = classesList1000G.count("SVA")
 Sva1000G = totalSva1000G - SvaBoth
 
-# Plot venn diagram
-ax1 = fig.add_subplot(2, 2, 3)
-ax1.set_title("SVA", fontsize=10)
+# Make venn diagram
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.set_title("SVA", fontsize=10)
 venn2(subsets=(SvaPCAWG, Sva1000G, SvaBoth), set_labels = ('', ''))
 
 ## ERVK
@@ -410,9 +475,9 @@ ERVKBoth = sum(1 for VCFlineObj in VCFlineObjList if (VCFlineObj.infoDict['CLASS
 totalERVK1000G = classesList1000G.count("ERVK")
 ERVK1000G = totalERVK1000G - ERVKBoth
 
-# Plot venn diagram
-ax1 = fig.add_subplot(2, 2, 4)
-ax1.set_title("ERVK", fontsize=10)
+# Make venn diagram
+ax4 = fig.add_subplot(2, 2, 4)
+ax4.set_title("ERVK", fontsize=10)
 venn2(subsets=(ERVKPCAWG, ERVK1000G, ERVKBoth), set_labels = ('', ''))
 
 ## Make legend 
