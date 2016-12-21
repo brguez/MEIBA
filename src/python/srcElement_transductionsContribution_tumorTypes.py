927498f4467e7cce15fbeffcb880ccd051b9de2f
brguez <rodriguezmartinbernardo@gmail.com>
2016-12-21 12:39:36 +0100

    srcElement_transductionsContribution_tumorTypes.py Improved the script for making a bar plot with the transduction contribution of each source element and the total number of transductions mediated by each element

--- src/python/srcElement_transductionsContribution_tumorTypes.py
+++ src/python/srcElement_transductionsContribution_tumorTypes.py
@@ -74,7 +74,6 @@ dfAlleleFreqAncestry = pd.read_csv(alleleFreqFile, header=0, index_col=0, sep='\
 # Convert allele frequencies into percentages (easier to interpret...)
 dfAlleleFreqAncestry = dfAlleleFreqAncestry * 100
 
-
 #### 3) Compute the contribution (%) of each source element to the total number of
 ####################################################################################
 # transductions in each tumor type
@@ -131,6 +130,7 @@ for srcElementId in srcElementIds:
 ### Compute the total number of transductions in PCAWG cohort
 totalNbTransductionsPCAWG = projectCodeNbTransductionsSerie.sum(axis=0)
 
+print "total-nb-td-PCAWG: ", totalNbTransductionsPCAWG
 ### Add the total number of transductions in PCAWG to the serie with total number of transductions per tumor type
 projectCodeNbTransductionsFilteredSerie["PCAWG"] = totalNbTransductionsPCAWG
 
@@ -171,16 +171,17 @@ nbSrcElementsOther = len(dfSrcElementContributionOther.index)
 
 otherContributionSerie = dfSrcElementContributionOther.sum(axis=0)
 
-### Add the Other category to the dataframe:
+### Make final dataframe and add the other category as an additional row
 rowName = "Other(" + str(nbSrcElementsOther)  + ")" 
-dfSrcElementContributionFiltered.loc[rowName] = otherContributionSerie
-print "final-contribution: ", dfSrcElementContributionFiltered
+
+dfSrcElementContributionFinal = dfSrcElementContributionFiltered
+dfSrcElementContributionFinal.loc[rowName] = otherContributionSerie
+print "final-contribution: ", dfSrcElementContributionFinal
 
 ##### 5.2 Source element allele frequency df 
-
 #### Filter dataframe to only have those src elements selected in the contribution dataframe  
 # Also, sort them in the same step to have the elements in the same order...
-colOrder = dfSrcElementContributionFiltered.index.tolist()
+colOrder = dfSrcElementContributionFinal.index.tolist()
 colOrder.pop() # Remove last element as it is 'Other' category...
 
 dfAlleleFreqAncestryFiltered = dfAlleleFreqAncestry.loc[colOrder, :]
@@ -192,13 +193,44 @@ dfAlleleFreqAncestryFiltered = dfAlleleFreqAncestryFiltered.loc[: , rowOrder]
 
 print "final-ancestry: ", dfAlleleFreqAncestryFiltered
 
+##### 5.3 Source element total number of transductions serie 
+### Sort source elements (rows) in descending order of transductions contribution to PCAWG 
+# Filter dataframe to only have those src elements selected in the contribution dataframe  
+# Also, sort them in the same step to have the elements in the same order...
+colOrder = dfSrcElementContributionFiltered.index.tolist()
+
+colOrder.pop() # Remove last element as it is 'Other' category...
+
+srcElementNbTransductionsSerieFiltered = srcElementNbTransductionsSerie.loc[colOrder]
+
+### Group those elements contributing less than 1% in the category 'Other'
+otherSrcElementNbTransductionsSerie = srcElementNbTransductionsSerie.drop(colOrder)
+
+totalNbTransductionsOther = otherSrcElementNbTransductionsSerie.sum(axis=0)
+
+### Add the 'Other' category to make a final series 
+indexId = "Other(" + str(nbSrcElementsOther)  + ")" 
+
+srcElementNbTransductionsSerieFinal = srcElementNbTransductionsSerieFiltered
+srcElementNbTransductionsSerieFinal.loc[indexId] = totalNbTransductionsOther
+
+print "totalNbTransductionsFinal: ", srcElementNbTransductionsSerieFinal
+
+#df[~srcElementNbTransductionsSerie.isin(colOrder)]
+
+# dfSrcElementContributionOther = dfSrcElementContributionSorted.loc[dfSrcElementContributionSorted['PCAWG'] < 1]
+
+# nbSrcElementsOther = len(dfSrcElementContributionOther.index)
+
+# otherContributionSerie = dfSrcElementContributionOther.sum(axis=0)
+
 #### 6) Number of active source elements per tumor type
 #######################################################
 
 ### Compute the number of active source elements per tumor type
 projectCodeNbActiveSrcElementsSerie = dfTransductionsBinary.sum(axis=0)
 
-print "nb-active-src-elements: ", projectCodeNbActiveSrcElementsSerie
+#print "nb-active-src-elements: ", projectCodeNbActiveSrcElementsSerie
 
 ### Filter and order project codes
 colOrder = projectCodeNbTransductionsSortedSerie.index.tolist()
@@ -213,7 +245,7 @@ projectCodeNbActiveSrcElementsSerieFiltered = projectCodeNbActiveSrcElementsSeri
 ### Source element contribution to tumor type transductions total count
 
 # Save output into tsv
-outFilePath = outDir + '/germline_source_element_contribution_perTumorType.tsv'
+outFilePath = outDir + '/germline_source_element_contribution_perTumorType2.tsv'
 dfSrcElementContribution.to_csv(outFilePath, sep='\t') 
 
 #### 8) Make plots
@@ -225,18 +257,20 @@ sns.set_style("whitegrid")
 ##### 8.1 Prepare Data
 
 ### transpose dataframes to have source elements as columns and tumor types as rows
-dfSrcElementContributionFiltered = dfSrcElementContributionFiltered.T 
+dfSrcElementContributionFinal = dfSrcElementContributionFinal.T 
 dfAlleleFreqAncestryFiltered = dfAlleleFreqAncestryFiltered.T
 
+
 ### Convert dataframe values into floats 
-dfSrcElementContributionFiltered = dfSrcElementContributionFiltered.apply(lambda x: pd.to_numeric(x))
+dfSrcElementContributionFinal = dfSrcElementContributionFinal.apply(lambda x: pd.to_numeric(x))
+dfAlleleFreqAncestryFiltered =  dfAlleleFreqAncestryFiltered.apply(lambda x: pd.to_numeric(x))
 dfAlleleFreqAncestryFiltered =  dfAlleleFreqAncestryFiltered.apply(lambda x: pd.to_numeric(x))
 
 # Save ordered dataframes into tsv
-outFilePath = outDir + '/germline_source_element_contribution_perTumorType_ordered.tsv'
-dfSrcElementContributionFiltered.to_csv(outFilePath, sep='\t') 
+outFilePath = outDir + '/germline_source_element_contribution_perTumorType_ordered2.tsv'
+dfSrcElementContributionFinal.to_csv(outFilePath, sep='\t') 
 
-outFilePath = outDir + '/germline_source_element_alleleFreq_perAncestry_ordered.tsv'
+outFilePath = outDir + '/germline_source_element_alleleFreq_perAncestry_ordered2.tsv'
 dfAlleleFreqAncestryFiltered.to_csv(outFilePath, sep='\t') 
 
 ####### 8.2 Make heatmaps
@@ -244,7 +278,7 @@ dfAlleleFreqAncestryFiltered.to_csv(outFilePath, sep='\t')
 ##### A) Contribution heatmap
 fig = plt.figure(figsize=(17,7))
 fig.suptitle('')
-ax1 = sns.heatmap(dfSrcElementContributionFiltered, vmin=0, vmax=50, annot=True, fmt=".1f", linewidths=.5, cmap=plt.cm.Oranges, annot_kws={"size": 8}, square=True)
+ax1 = sns.heatmap(dfSrcElementContributionFinal, vmin=0, vmax=50, annot=True, fmt=".1f", linewidths=.5, cmap=plt.cm.Oranges, annot_kws={"size": 8}, square=True)
 
 ax1.set_xlabel('')
 ax1.set_ylabel('')
@@ -282,19 +316,57 @@ for item in ax2.get_xticklabels():
 fileName = outDir + "/source_element_alleleFreq_ancestry_heatmap6.pdf"
 plt.savefig(fileName)
 
-####### 8.2 Make barplot
+####### 8.2 Make barplots
+
+##### A) Global contribution (%) to PCAWG
+
+# Prepare series for plotting
+totalSrcElementContributionSeries = dfSrcElementContributionFinal.loc['PCAWG', :]
+
+print "test: ", totalSrcElementContributionSeries
+
+# Initialize the matplotlib figure
+fig = plt.figure(figsize=(12, 6))
+
+# Plot the contribution of each source element to the total number of retrotransposition insertions across PCAWG
+ax3 = totalSrcElementContributionSeries.plot(kind='bar')
+ax3.set(ylim=(0, 26), ylabel="Somatic retrotransposition contribution (%)")
+
+# turn the axis labels
+for item in ax3.get_yticklabels():
+    item.set_rotation(0)
+
+for item in ax3.get_xticklabels():
+    item.set_rotation(45)
+
+# Add to the top of each bar the total number of retrotransposition events mediated by each src element
+rects = ax3.patches
+labels = srcElementNbTransductionsSerieFinal.values
+
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax3.text(rect.get_x() + rect.get_width()/2, height, label, ha='center', va='bottom', size='10')
+
+## Save figure 
+fileName = outDir + "/source_element_global_contribution_barplot.pdf"
+plt.savefig(fileName)
+
+
+##### B) Number of active source elements per tumor type barplot
+
 # Initialize the matplotlib figure
 fig = plt.figure(figsize=(6,8))
 
 # Plot number of active source elements per tumor type
-ax3 = projectCodeNbActiveSrcElementsSerieFiltered.plot(kind="barh")
-ax3.invert_yaxis()
+ax4 = projectCodeNbActiveSrcElementsSerieFiltered.plot(kind="barh")
+ax4.invert_yaxis()
 
-
-ax3.set(xlim=(0, 75), ylabel="",
+ax4.set(xlim=(0, 75), ylabel="",
        xlabel="# Active Source Elements")
 
 ## Save figure 
 fileName = outDir + "/source_element_barplot.pdf"
 plt.savefig(fileName)
+
+
 
