1d75076bd6a9fc4513501a4b93ba4288a207384d
brguez <rodriguezmartinbernardo@gmail.com>
2017-04-17 16:59:36 +0100

    extract_reads_1000bp_mates.sh Minor edits minibam script

--- src/bash/extract_reads_1000bp_mates.sh
+++ src/bash/extract_reads_1000bp_mates.sh
@@ -17,23 +17,28 @@ while read CHROM POS CLASS PROJECT DONOR; do
     echo -e "\nProcessing: $CHROM $POS $CLASS $PROJECT $DONOR"
     
     # Get path to BAM file
-    BAM=`grep -P "^$DONOR\t" $bamPaths | cut -f2`
+    echo "awk '$1==$DONOR' $bamPaths | cut -f2"    
+    BAM=`awk '$1==$DONOR' $bamPaths | cut -f2`
     echo "BAM file: $BAM"
     
     # Extract read IDs in interval of +/-1000 bp
     START=$(( POS - 1000 ))
     END=$(( POS + 1000 ))
     echo "Running samtools view in interval ${CHROM}:${START}-${END}"
-    samtools view $BAM ${CHROM}:${START}-${END} | cut -f1 > $outDir/${CHROM}_${POS}_${CLASS}.reads
+    echo "samtools view $BAM ${CHROM}:${START}-${END} | cut -f1 > $outDir/${CHROM}_${POS}_${CLASS}.reads"
+    #samtools view $BAM ${CHROM}:${START}-${END} | cut -f1 > $outDir/${CHROM}_${POS}_${CLASS}.reads
     NUMREADS=`cat $outDir/${CHROM}_${POS}_${CLASS}.reads | wc -l`
     NUMREADS=$(( NUMREADS * 2 ))
     
     # Extract all the reads and their mates
     echo "Extracting all found reads and their mates ($NUMREADS)"
-    samtools view -H $BAM > $outDir/${CHROM}_${POS}_${CLASS}.sam
-    samtools view $BAM | grep -m $NUMREADS -Ff $outDir/${CHROM}_${POS}_${CLASS}.reads >> $outDir/${CHROM}_${POS}_${CLASS}.sam
+    echo "samtools view -H $BAM > $outDir/${CHROM}_${POS}_${CLASS}.sam"    
+    #samtools view -H $BAM > $outDir/${CHROM}_${POS}_${CLASS}.sam
+    echo "samtools view $BAM | grep -m $NUMREADS -Ff $outDir/${CHROM}_${POS}_${CLASS}.reads >> $outDir/${CHROM}_${POS}_${CLASS}.sam" 
+    #samtools view $BAM | grep -m $NUMREADS -Ff $outDir/${CHROM}_${POS}_${CLASS}.reads >> $outDir/${CHROM}_${POS}_${CLASS}.sam
     echo "Compressing to BAM"
-    samtools view -Sb $outDir/${CHROM}_${POS}_${CLASS}.sam > $outDir/${CHROM}_${POS}_${CLASS}.bam
+    echo "samtools view -Sb $outDir/${CHROM}_${POS}_${CLASS}.sam > $outDir/${CHROM}_${POS}_${CLASS}.bam"    
+    #samtools view -Sb $outDir/${CHROM}_${POS}_${CLASS}.sam > $outDir/${CHROM}_${POS}_${CLASS}.bam
     rm $outDir/${CHROM}_${POS}_${CLASS}.reads $outDir/${CHROM}_${POS}_${CLASS}.sam
     echo "Done"
     
