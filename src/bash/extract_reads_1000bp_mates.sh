6390fe199d675a29b3c0bff39bcf74d74481d317
brguez <rodriguezmartinbernardo@gmail.com>
2017-04-17 17:13:10 +0100

    Minor edit minibam script

--- src/bash/extract_reads_1000bp_mates.sh
+++ src/bash/extract_reads_1000bp_mates.sh
@@ -4,11 +4,11 @@
 # insertion position, AND their mates
 
 # $1: tab-delimited file with no header and columns: chrom, pos, class, projectCode, donorId
-# $2: tab-delimited file with no header and columns: donorId, bamFile
+# $2: BAM file path
 # $3: output directory
 
 input=$1
-bamPaths=$2
+BAM=$2
 outDir=$3 
 
 # For each insertion
@@ -17,8 +17,6 @@ while read CHROM POS CLASS PROJECT DONOR; do
     echo -e "\nProcessing: $CHROM $POS $CLASS $PROJECT $DONOR"
     
     # Get path to BAM file
-    echo "awk '$1==$DONOR' $bamPaths | cut -f2"    
-    BAM=`awk '$1==$DONOR' $bamPaths | cut -f2`
     echo "BAM file: $BAM"
     
     # Extract read IDs in interval of +/-1000 bp
@@ -26,19 +24,19 @@ while read CHROM POS CLASS PROJECT DONOR; do
     END=$(( POS + 1000 ))
     echo "Running samtools view in interval ${CHROM}:${START}-${END}"
     echo "samtools view $BAM ${CHROM}:${START}-${END} | cut -f1 > $outDir/${CHROM}_${POS}_${CLASS}.reads"
-    #samtools view $BAM ${CHROM}:${START}-${END} | cut -f1 > $outDir/${CHROM}_${POS}_${CLASS}.reads
+    samtools view $BAM ${CHROM}:${START}-${END} | cut -f1 > $outDir/${CHROM}_${POS}_${CLASS}.reads
     NUMREADS=`cat $outDir/${CHROM}_${POS}_${CLASS}.reads | wc -l`
     NUMREADS=$(( NUMREADS * 2 ))
     
     # Extract all the reads and their mates
     echo "Extracting all found reads and their mates ($NUMREADS)"
     echo "samtools view -H $BAM > $outDir/${CHROM}_${POS}_${CLASS}.sam"    
-    #samtools view -H $BAM > $outDir/${CHROM}_${POS}_${CLASS}.sam
+    samtools view -H $BAM > $outDir/${CHROM}_${POS}_${CLASS}.sam
     echo "samtools view $BAM | grep -m $NUMREADS -Ff $outDir/${CHROM}_${POS}_${CLASS}.reads >> $outDir/${CHROM}_${POS}_${CLASS}.sam" 
-    #samtools view $BAM | grep -m $NUMREADS -Ff $outDir/${CHROM}_${POS}_${CLASS}.reads >> $outDir/${CHROM}_${POS}_${CLASS}.sam
+    samtools view $BAM | grep -m $NUMREADS -Ff $outDir/${CHROM}_${POS}_${CLASS}.reads >> $outDir/${CHROM}_${POS}_${CLASS}.sam
     echo "Compressing to BAM"
     echo "samtools view -Sb $outDir/${CHROM}_${POS}_${CLASS}.sam > $outDir/${CHROM}_${POS}_${CLASS}.bam"    
-    #samtools view -Sb $outDir/${CHROM}_${POS}_${CLASS}.sam > $outDir/${CHROM}_${POS}_${CLASS}.bam
+    samtools view -Sb $outDir/${CHROM}_${POS}_${CLASS}.sam > $outDir/${CHROM}_${POS}_${CLASS}.bam
     rm $outDir/${CHROM}_${POS}_${CLASS}.reads $outDir/${CHROM}_${POS}_${CLASS}.sam
     echo "Done"
     
