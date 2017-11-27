f331b41eb18920d5a1b40e31307fe9393e6c9624
brguez <rodriguezmartinbernardo@gmail.com>
2017-11-27 10:53:09 +0100

    vcf2bed.py Improvements to make it more flexible

--- src/python/vcf2bed.py
+++ src/python/vcf2bed.py
@@ -38,13 +38,17 @@ import formats
 
 ## Get user's input ##
 parser = argparse.ArgumentParser(description= """""")
-parser.add_argument('vcf', help='Multisample VCF containing genotyped MEI')
+parser.add_argument('vcf', help='VCF containing MEI')
 parser.add_argument('fileName', help='Output file name')
+parser.add_argument('--overHang', default=500, dest='overHang', type=int, help='Bed coordinates will be defined as (beg-overHang) and (end+overHang). Default: 500' )
+parser.add_argument('--filter', action='store_true', dest='filter', help='Flag to filter out insertions that do have PASS filtering status.')
 parser.add_argument('-o', '--outDir', default=os.getcwd(), dest='outDir', help='output directory. Default: current working directory.' )
 
 args = parser.parse_args()
 inputVCF = args.vcf
 fileName = args.fileName
+overHang = args.overHang
+filterBool = args.filter
 outDir = args.outDir
 
 scriptName = os.path.basename(sys.argv[0])
@@ -54,10 +58,13 @@ print
 print "***** ", scriptName, " configuration *****"
 print "vcf: ", inputVCF
 print "fileName: ", fileName
+print "overHang: ", overHang
+print "filterBool: ", filterBool
 print "outDir: ", outDir
 print
 print "***** Executing ", scriptName, ".... *****"
 print
+
 
 ## Start ## 
 
@@ -77,23 +84,24 @@ outFilePath = outDir + '/' + fileName + '.bed'
 outFile = open(outFilePath, 'w')
 
 # Write header:
-row = '#chrom' + "\t" + 'beg' +  "\t" + 'end' + "\t" + 'iClass' + "\t" + 'iType' + "\t" + 'srcId' + "\n"
+row = '#chrom' + "\t" + 'beg' +  "\t" + 'end' + "\t" + 'iClass' + "\t" + 'iType' + "\t" + 'srcId' + '\t' + "GR" + "\n"
 outFile.write(row)
 
 ## For each MEI
 for MEIObj in VCFObj.lineList:
 
-    ## Select only those MEI that passes all the filters
-    if (MEIObj.filter == "PASS"):
+    ## If filter enabled select only those MEI that passes all the filters
+    # If not enabled select all the MEI
+    if (filterBool == False) or (filterBool == True and MEIObj.filter == "PASS"):
 
         chrom = MEIObj.chrom
-        beg = str(MEIObj.pos - 1000)
-        end = str(MEIObj.pos + 1000)
+        beg = str(MEIObj.pos - overHang)
+        end = str(int(MEIObj.infoDict['BKPB']) + overHang) if 'BKPB' in MEIObj.infoDict else str(MEIObj.pos + overHang)
         iClass = MEIObj.infoDict["CLASS"]
         iType = MEIObj.infoDict["TYPE"]
-        srcId = MEIObj.infoDict['SRCID'] if 'SRCID' in MEIObj.infoDict else 'NA'
-    
-        row = chrom + '\t' + beg + '\t' + end + '\t' + iClass + '\t' + iType + '\t' + srcId + '\n'
+        srcId = MEIObj.infoDict['SRCID'] if 'SRCID' in MEIObj.infoDict else 'NA' 
+        GR = MEIObj.infoDict['GR'] if 'GR' in MEIObj.infoDict else 'NA' 
+        row = chrom + '\t' + beg + '\t' + end + '\t' + iClass + '\t' + iType + '\t' + srcId + '\t' + GR + '\n'
         outFile.write(row)
        
 
