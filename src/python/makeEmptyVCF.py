bc58ac0d549d54920c6dc73835a744a702fd6788
brguez <rodriguezmartinbernardo@gmail.com>
2018-01-11 10:31:26 +0100

    makeEmptyVCF.py Fixed issue with method name

--- src/python/makeEmptyVCF.py
+++ src/python/makeEmptyVCF.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     ## 1. Create VCF object and print VCF header    
     header("Creating VCF object and printing VCF header into the output file")
     VCFObj = formats.VCF()
-    VCFObj.print_header(outFilePath)
+    VCFObj.write_header(outFilePath)
 
     ## Finish ##
     print
